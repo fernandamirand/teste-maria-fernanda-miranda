fb26501d6b96f27ef87704d8d46c1ee1008459ab
fernandamirand <imfernandamiranda@gmail.comclear>
2022-06-22 00:42:09 -0300

    add-map-base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import { MapContainer, 
+         TileLayer, 
+         useMap,
+         Marker,
+         Popup } 
+from 'react-leaflet'
+import './App.css'
+import equipmentPositionHistory from './data/equipmentPositionHistory.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Header/>
+       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {equipmentPositionHistory.map(equipment=>(
+              
+                <Marker 
+                  key={equipment.id}
+                  position={[51.505, -0.09]}
+                  >
+                </Marker>
+
+            ))}
+          
+       </MapContainer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
